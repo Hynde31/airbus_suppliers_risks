d19fc67adbb9dafe9678409b2aa543a821157257
Hynde31 <hynde313@gmail.com>
2025-05-27 18:18:32 +0200

    Update airbus_dashboard.py

--- airbus_dashboard.py
+++ airbus_dashboard.py
@@ -3,6 +3,13 @@ import pandas as pd
 import os
 from suppliers_data import SUPPLIERS
 
+st.set_page_config(page_title="Airbus Digital Twin Supply Chain Dashboard", layout="wide")
+
+import streamlit as st
+import pandas as pd
+import os
+from suppliers_data import SUPPLIERS
+
 # --------- DATA FLATTENING ---------
 def flatten_suppliers(suppliers):
     rows = []
