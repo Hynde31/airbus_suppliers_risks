51953264bee90ae6990234d6172b28fa4298061f
Hynde31 <hynde313@gmail.com>
2025-05-28 11:22:40 +0200

    Update airbus_risk_dashboard.py

--- airbus_risk_dashboard.py
+++ airbus_risk_dashboard.py
@@ -196,5 +196,5 @@ else:
 # --------- FOOTER ---------
 st.markdown("""
 ---
-Developed for Airbus - by Hynde EL HOUJJAJI · [2025]
+Developed for Airbus Commercial Aircraft - by Hynde EL HOUJJAJI - Subcontractor for Capgemini Engineering· [2025]
 """)
